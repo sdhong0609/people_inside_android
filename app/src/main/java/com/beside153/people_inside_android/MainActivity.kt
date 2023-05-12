6ef2b1684ce57e6e41645fbef902963e1a4ec383
hong <sdhong0609@gmail.com>
2023-05-12 17:45:28 +0900

    chore: 프로젝트 생성

---
+++ app/src/main/java/com/beside153/people_inside_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.beside153.people_inside_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
