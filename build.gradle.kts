5fab0eb8366eef8f25dc600ea09b46f7952a7cfd
hong <sdhong0609@gmail.com>
2023-05-22 13:03:08 +0900

    fix: project-level build.gradle.kts 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,10 @@ plugins {
     id("com.android.application") version "8.0.1" apply false
     id("com.android.library") version "8.0.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
-    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 buildscript {
     dependencies {
-        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.android.tools.build:gradle:8.0.1")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
