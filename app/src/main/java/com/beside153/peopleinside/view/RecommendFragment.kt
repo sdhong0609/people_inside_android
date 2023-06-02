69ff584736fdd636a59476addec2c0ae388e6905
hong <sdhong0609@gmail.com>
2023-06-02 20:05:43 +0900

    feature: app bar 초기 구현, BNV 아이콘 추가

--- app/src/main/java/com/beside153/peopleinside/view/RecommendFragment.kt
+++ app/src/main/java/com/beside153/peopleinside/view/RecommendFragment.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.beside153.peopleinside.R
+import com.beside153.peopleinside.databinding.FragmentRecommendBinding
 
 class RecommendFragment : Fragment() {
+    private lateinit var binding: FragmentRecommendBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommend, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_recommend, container, false)
+        return binding.root
     }
 }
