b81b55e64fe550875c75f1becce459ef0b821e33
hong <sdhong0609@gmail.com>
2023-06-07 01:12:54 +0900

    feature: 커스텀하게 토스트 완성

--- app/src/main/java/com/beside153/peopleinside/view/CreateReviewActivity.kt
+++ app/src/main/java/com/beside153/peopleinside/view/CreateReviewActivity.kt
@@ -3,6 +3,10 @@ package com.beside153.peopleinside.view
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.beside153.peopleinside.R
@@ -18,9 +22,19 @@ class CreateReviewActivity : AppCompatActivity() {
         binding.buttonClose.setOnClickListener {
             finish()
         }
+
+        binding.buttonComplete.setOnClickListener {
+            val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.buttonComplete.windowToken, 0)
+            binding.textViewCompleteReview.visibility = View.VISIBLE
+            Handler(Looper.getMainLooper()).postDelayed({
+                finish()
+            }, DURATION_UNTIL_BACK)
+        }
     }
 
     companion object {
+        private const val DURATION_UNTIL_BACK = 2000L
 
         fun createReviewIntent(context: Context): Intent {
             return Intent(context, CreateReviewActivity::class.java)
