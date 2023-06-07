fb740c39fbdc39d85266e8da135314f1c9b4d397
hong <sdhong0609@gmail.com>
2023-06-07 23:24:38 +0900

    feature: 스플래시 화면 구현

---
+++ app/src/main/java/com/beside153/peopleinside/view/login/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.beside153.peopleinside.view.login
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.beside153.peopleinside.view.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_DURATION)
+    }
+
+    companion object {
+        private const val SPLASH_DURATION = 1000L
+    }
+}
