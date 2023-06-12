f97e938eb1e06dca014c231ff1e8f55e949b6d40
hong <sdhong0609@gmail.com>
2023-06-13 00:30:29 +0900

    feature: 사용자 정보 등록 화면으로 이동 구현

--- app/src/main/java/com/beside153/peopleinside/view/login/SignUpTermsFragment.kt
+++ app/src/main/java/com/beside153/peopleinside/view/login/SignUpTermsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.beside153.peopleinside.R
 import com.beside153.peopleinside.databinding.FragmentSignUpTermsBinding
 
@@ -19,5 +20,14 @@ class SignUpTermsFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_up_terms, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.agreeTermsButton.setOnClickListener {
+            val action = SignUpTermsFragmentDirections.actionSignUpTermsFragmentToSignUpUserInfoFragment()
+            findNavController().navigate(action)
+        }
     }
 }
---
+++ app/src/main/java/com/beside153/peopleinside/view/login/SignUpUserInfoFragment.kt
@@ -0,0 +1,20 @@
+package com.beside153.peopleinside.view.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.beside153.peopleinside.R
+
+class SignUpUserInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up_user_info, container, false)
+    }
+}
