e765ac7bd2b9206be1875e4623e4ba37181fc8e2
hong <sdhong0609@gmail.com>
2023-06-16 14:50:42 +0900

    feature: 다크모드 제한

---
+++ app/src/main/java/com/beside153/peopleinside/view/App.kt
@@ -0,0 +1,11 @@
+package com.beside153.peopleinside.view
+
+import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
