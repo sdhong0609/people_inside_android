5da7c3183dcb4ab2619fd75157a6382578d9ae6e
hong <sdhong0609@gmail.com>
2023-06-18 19:20:48 +0900

    fix: MyPageFragment 패키지 변경, databinding 적용

--- app/src/main/java/com/beside153/peopleinside/view/mypage/MyPageFragment.kt
+++ app/src/main/java/com/beside153/peopleinside/view/mypage/MyPageFragment.kt
@@ -1,20 +1,23 @@
-package com.beside153.peopleinside.view
+package com.beside153.peopleinside.view.mypage
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.beside153.peopleinside.R
+import com.beside153.peopleinside.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
+    private lateinit var binding: FragmentMyPageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false)
+        return binding.root
     }
 }
