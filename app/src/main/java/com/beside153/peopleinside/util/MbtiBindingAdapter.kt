c33506e684c1380abb10ca5df8ab6343d8200012
hong <sdhong0609@gmail.com>
2023-06-23 22:28:40 +0900

    feat: mbti 영화배틀정보 진행중

--- app/src/main/java/com/beside153/peopleinside/util/MbtiBindingAdapter.kt
+++ app/src/main/java/com/beside153/peopleinside/util/MbtiBindingAdapter.kt
@@ -31,3 +31,31 @@ fun ImageView.mbtiCircleImg(mbti: String) {
 
     setImageResource(imgRes)
 }
+
+@Suppress("CyclomaticComplexMethod")
+@BindingAdapter("mbtiHeartImg")
+fun ImageView.mbtiHeartImg(mbti: String) {
+    val imgRes = when (mbti) {
+        "ENFJ" -> R.drawable.mbti_heart_icon_enfj
+        "ENFP" -> R.drawable.mbti_heart_icon_enfp
+        "ENTJ" -> R.drawable.mbti_heart_icon_entj
+        "ENTP" -> R.drawable.mbti_heart_icon_entp
+        "ESFJ" -> R.drawable.mbti_heart_icon_esfj
+        "ESFP" -> R.drawable.mbti_heart_icon_esfp
+        "ESTJ" -> R.drawable.mbti_heart_icon_estj
+        "ESTP" -> R.drawable.mbti_heart_icon_estp
+
+        "INFJ" -> R.drawable.mbti_heart_icon_infj
+        "INFP" -> R.drawable.mbti_heart_icon_infp
+        "INTJ" -> R.drawable.mbti_heart_icon_intj
+        "INTP" -> R.drawable.mbti_heart_icon_intp
+        "ISFJ" -> R.drawable.mbti_heart_icon_isfj
+        "ISFP" -> R.drawable.mbti_heart_icon_isfp
+        "ISTJ" -> R.drawable.mbti_heart_icon_istj
+        "ISTP" -> R.drawable.mbti_heart_icon_istp
+
+        else -> R.drawable.mbti_heart_icon_enfj
+    }
+
+    setImageResource(imgRes)
+}
