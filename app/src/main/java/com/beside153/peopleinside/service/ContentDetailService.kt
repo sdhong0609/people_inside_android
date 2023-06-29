c5468005195587c323a837990a4758cb8537de75
hong <sdhong0609@gmail.com>
2023-06-29 14:38:31 +0900

    feat: 별점(평가)관련 service에 코드작성

--- app/src/main/java/com/beside153/peopleinside/service/ContentDetailService.kt
+++ app/src/main/java/com/beside153/peopleinside/service/ContentDetailService.kt
@@ -3,7 +3,12 @@ package com.beside153.peopleinside.service
 import com.beside153.peopleinside.model.contentdetail.ContentCommentModel
 import com.beside153.peopleinside.model.contentdetail.ContentDetailModel
 import com.beside153.peopleinside.model.contentdetail.ContentRatingModel
+import com.beside153.peopleinside.model.contentdetail.ContentRatingRequest
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -19,4 +24,19 @@ interface ContentDetailService {
 
     @GET("/api/media-content/{contentId}/rating/rater/{raterId}")
     suspend fun getContentRating(@Path("contentId") contentId: Int, @Path("raterId") raterId: Int): ContentRatingModel
+
+    @POST("/api/media-content/{contentId}/rating")
+    suspend fun postContentRating(
+        @Path("contentId") contentId: Int,
+        @Body contentRatingRequest: ContentRatingRequest
+    ): ContentRatingModel
+
+    @PUT("/api/media-content/{contentId}/rating")
+    suspend fun putContentRating(
+        @Path("contentId") contentId: Int,
+        @Body contentRatingRequest: ContentRatingRequest
+    ): ContentRatingModel
+
+    @DELETE("/api/media-content/{contentId}/rating/{ratingId}")
+    suspend fun deleteContentRating(@Path("contentId") contentId: Int, @Path("raterId") raterId: Int): Boolean
 }
