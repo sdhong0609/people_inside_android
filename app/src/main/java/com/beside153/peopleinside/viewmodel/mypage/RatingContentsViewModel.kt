5de701cbba1e2d9e98173baf21533f8d061689bd
hong <sdhong0609@gmail.com>
2023-07-03 00:10:22 +0900

    feat: 내가 평가한 콘텐츠 Viewmodel 생성

--- app/src/main/java/com/beside153/peopleinside/viewmodel/mypage/RatingContentsViewModel.kt
+++ app/src/main/java/com/beside153/peopleinside/viewmodel/mypage/RatingContentsViewModel.kt
@@ -7,17 +7,28 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.beside153.peopleinside.base.BaseViewModel
+import com.beside153.peopleinside.model.mypage.RatingContentModel
 import com.beside153.peopleinside.service.MyContentService
 import com.beside153.peopleinside.service.RetrofitClient
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class RatingContentsViewModel(private val myContentService: MyContentService) : BaseViewModel() {
     private val _ratingCount = MutableLiveData(0)
     val ratingCount: LiveData<Int> get() = _ratingCount
 
+    private val _contentList = MutableLiveData<List<RatingContentModel>>()
+    val contentList: LiveData<List<RatingContentModel>> get() = _contentList
+
+    private var page = 1
+
     fun initAllData() {
         viewModelScope.launch(exceptionHandler) {
-            _ratingCount.value = myContentService.getRatingCount()
+            val ratingCountDeferred = async { myContentService.getRatingCount() }
+            val contentListDeferred = async { myContentService.getRatingContents(page) }
+
+            ratingCountDeferred.await()
+            contentListDeferred.await()
         }
     }
 
