3ad079377538e1e072ec49fa0c0fd4ef6e7b67fc
hong <sdhong0609@gmail.com>
2023-07-03 22:50:23 +0900

    feat: 내가 평가한 콘텐츠 진행중

--- app/src/main/java/com/beside153/peopleinside/viewmodel/mypage/RatingContentsViewModel.kt
+++ app/src/main/java/com/beside153/peopleinside/viewmodel/mypage/RatingContentsViewModel.kt
@@ -7,13 +7,20 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.beside153.peopleinside.base.BaseViewModel
+import com.beside153.peopleinside.model.contentdetail.ContentRatingRequest
+import com.beside153.peopleinside.model.mypage.Rating
 import com.beside153.peopleinside.model.mypage.RatingContentModel
+import com.beside153.peopleinside.service.ContentDetailService
 import com.beside153.peopleinside.service.MyContentService
 import com.beside153.peopleinside.service.RetrofitClient
+import com.beside153.peopleinside.util.roundToHalf
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
-class RatingContentsViewModel(private val myContentService: MyContentService) : BaseViewModel() {
+class RatingContentsViewModel(
+    private val myContentService: MyContentService,
+    private val contentDetailService: ContentDetailService
+) : BaseViewModel() {
     private val _ratingCount = MutableLiveData(0)
     val ratingCount: LiveData<Int> get() = _ratingCount
 
@@ -32,7 +39,35 @@ class RatingContentsViewModel(private val myContentService: MyContentService) : 
         }
     }
 
+    fun onRatingChanged(rating: Float, item: RatingContentModel) {
+        viewModelScope.launch(exceptionHandler) {
+            val currentRating = item.rating.rating
+            if (currentRating.roundToHalf() == rating) return@launch
+
+            val currentRatingHasValue = 0 < currentRating && currentRating <= MAX_RATING
+            if (currentRating <= 0) {
+                contentDetailService.postContentRating(item.contentId, ContentRatingRequest(rating))
+            } else if (currentRatingHasValue && (0 < rating && rating <= MAX_RATING)) {
+                contentDetailService.putContentRating(item.contentId, ContentRatingRequest(rating))
+            } else if (currentRatingHasValue && rating == 0f) {
+                contentDetailService.deleteContentRating(item.contentId, item.rating.ratingId)
+            }
+
+            val updatedList = _contentList.value?.map {
+                if (item == it) {
+                    it.copy(rating = Rating(it.contentId, it.rating.ratingId, rating))
+                } else {
+                    it
+                }
+            }
+            _contentList.value = updatedList ?: emptyList()
+            _ratingCount.value = myContentService.getRatingCount()
+        }
+    }
+
     companion object {
+        private const val MAX_RATING = 5
+
         val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
             override fun <T : ViewModel> create(
@@ -40,7 +75,8 @@ class RatingContentsViewModel(private val myContentService: MyContentService) : 
                 extras: CreationExtras
             ): T {
                 val myContentService = RetrofitClient.myContentService
-                return RatingContentsViewModel(myContentService) as T
+                val contentDetailService = RetrofitClient.contentDetailService
+                return RatingContentsViewModel(myContentService, contentDetailService) as T
             }
         }
     }
