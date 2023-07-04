1051f32ed8deb0148e2675164043d45c53f9c3c7
hong <sdhong0609@gmail.com>
2023-07-05 01:28:25 +0900

    feat: 프로필 수정 activity 및 fragment 분할

---
+++ app/src/main/java/com/beside153/peopleinside/view/mypage/EditProfileFragment.kt
@@ -0,0 +1,20 @@
+package com.beside153.peopleinside.view.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.beside153.peopleinside.R
+
+class EditProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    }
+}
