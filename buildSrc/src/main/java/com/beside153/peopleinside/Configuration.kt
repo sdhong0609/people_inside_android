5a29e4b1590f6cf566f40ea4f52010095b9c4736
hong <sdhong0609@gmail.com>
2023-07-12 00:19:47 +0900

    versionCode 업데이트

--- buildSrc/src/main/java/com/beside153/peopleinside/Configuration.kt
+++ buildSrc/src/main/java/com/beside153/peopleinside/Configuration.kt
@@ -8,5 +8,5 @@ object Configuration {
     const val minorVersion = 0
     const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 10
+    const val versionCode = 11
 }
