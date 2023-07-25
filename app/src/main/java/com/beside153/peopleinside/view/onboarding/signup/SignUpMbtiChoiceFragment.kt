58ef124fa7858e6b637ab8ad7f0ecace7f1eac14
hong <sdhong0609@gmail.com>
2023-07-25 20:54:42 +0900

    fix: grid item decoration 제거

--- app/src/main/java/com/beside153/peopleinside/view/onboarding/signup/SignUpMbtiChoiceFragment.kt
+++ app/src/main/java/com/beside153/peopleinside/view/onboarding/signup/SignUpMbtiChoiceFragment.kt
@@ -14,8 +14,6 @@ import com.beside153.peopleinside.R
 import com.beside153.peopleinside.base.BaseFragment
 import com.beside153.peopleinside.databinding.FragmentSignUpMbtiChoiceBinding
 import com.beside153.peopleinside.model.common.MbtiModel
-import com.beside153.peopleinside.util.GridSpacingItemDecoration
-import com.beside153.peopleinside.util.dpToPx
 import com.beside153.peopleinside.view.onboarding.signup.MbtiScreenAdapter.MbtiScreenModel
 
 class SignUpMbtiChoiceFragment : BaseFragment() {
@@ -77,7 +75,6 @@ class SignUpMbtiChoiceFragment : BaseFragment() {
         binding.mbtiScreenRecyclerView.apply {
             adapter = mbtiAdapter
             layoutManager = gridLayoutManager
-            addItemDecoration(GridSpacingItemDecoration(16.dpToPx(resources.displayMetrics)))
 
             when (selectedMbtiItem?.mbtiText) {
                 "ENTP", "ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP", "ENTJ" -> scrollToPosition(mbtiList.size)
