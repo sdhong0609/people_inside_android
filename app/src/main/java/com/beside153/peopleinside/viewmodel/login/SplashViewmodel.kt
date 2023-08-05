f5fac7b281fcbeff154bba9529ac5b5cd40d9d4e
hong <sdhong0609@gmail.com>
2023-08-05 16:14:47 +0900

    refactor: 앱 강제 업데이트 로직 viewmodel에 추가

--- app/src/main/java/com/beside153/peopleinside/viewmodel/login/SplashViewmodel.kt
+++ app/src/main/java/com/beside153/peopleinside/viewmodel/login/SplashViewmodel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.beside153.peopleinside.App
+import com.beside153.peopleinside.BuildConfig
 import com.beside153.peopleinside.base.BaseViewModel
+import com.beside153.peopleinside.service.AppVersionService
 import com.beside153.peopleinside.service.ReportService
 import com.beside153.peopleinside.service.UserService
 import com.beside153.peopleinside.util.Event
@@ -17,18 +19,31 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewmodel @Inject constructor(
+    private val appVersionService: AppVersionService,
     private val reportService: ReportService,
     private val userService: UserService
-) :
-    BaseViewModel() {
+) : BaseViewModel() {
 
     private val _onBoardingCompletedEvent = MutableLiveData<Event<Boolean>>()
     val onBoardingCompletedEvent: LiveData<Event<Boolean>> get() = _onBoardingCompletedEvent
 
+    private val _updateAppEvent = MutableLiveData<Event<Unit>>()
+    val updateAppEvent: LiveData<Event<Unit>> get() = _updateAppEvent
+
     fun getAllData() {
         viewModelScope.launch(exceptionHandler) {
             val reportDeferred = async { reportService.getReportList() }
+            val appVersionDeferred = async { appVersionService.getAppVersionLatest("android") }
+
             val reportList = reportDeferred.await()
+            val requiredAppVersion = appVersionDeferred.await().requiredVersionName
+
+            val currentAppVersion = BuildConfig.VERSION_NAME
+
+            if (isNeedUpdate(currentAppVersion, requiredAppVersion)) {
+                _updateAppEvent.value = Event(Unit)
+                return@launch
+            }
 
             var onBoardingCompleted = true
             if (App.prefs.getUserId() != 0 && App.prefs.getIsMember()) {
@@ -44,5 +59,40 @@ class SplashViewmodel @Inject constructor(
             }
             _onBoardingCompletedEvent.value = Event(false)
         }
+    }
+
+    private fun isNeedUpdate(currentAppVersion: String, requiredAppVersion: String): Boolean {
+        var isNeedUpdate = false
+        val arrX = currentAppVersion.split(".")
+        val arrY = requiredAppVersion.split(".")
+
+        val length = maxOf(arrX.size, arrY.size)
+
+        for (i in 0 until length) {
+            val x: Int = try {
+                arrX.getOrNull(i)?.toInt() ?: 0
+            } catch (e: NumberFormatException) {
+                0
+            }
+            val y: Int = try {
+                arrY.getOrNull(i)?.toInt() ?: 0
+            } catch (e: NumberFormatException) {
+                0
+            }
+
+            if (x > y) {
+                // 앱 버전이 큼
+                isNeedUpdate = false
+                break
+            } else if (x < y) {
+                // 비교 버전이 큼
+                isNeedUpdate = true
+                break
+            } else {
+                // 버전 동일
+                isNeedUpdate = false
+            }
+        }
+        return isNeedUpdate
     }
 }
