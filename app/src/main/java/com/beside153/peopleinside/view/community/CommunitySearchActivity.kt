ed11bf0e9fd00fb8e580e0cee0e23f7c4d71fb91
hong <sdhong0609@gmail.com>
2023-08-11 18:54:26 +0900

    feat: 검색 결과 없음 구현

--- app/src/main/java/com/beside153/peopleinside/view/community/CommunitySearchActivity.kt
+++ app/src/main/java/com/beside153/peopleinside/view/community/CommunitySearchActivity.kt
@@ -3,25 +3,52 @@ package com.beside153.peopleinside.view.community
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.beside153.peopleinside.R
 import com.beside153.peopleinside.base.BaseActivity
 import com.beside153.peopleinside.databinding.ActivityCommunitySearchBinding
+import com.beside153.peopleinside.model.community.post.CommunityPostModel
 import com.beside153.peopleinside.util.EventObserver
+import com.beside153.peopleinside.util.setOpenActivityAnimation
 import com.beside153.peopleinside.viewmodel.community.CommunitySearchViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CommunitySearchActivity : BaseActivity() {
     private lateinit var binding: ActivityCommunitySearchBinding
     private val communitySearchViewModel: CommunitySearchViewModel by viewModels()
+    private val postListAdapter = CommunityPostListAdapter(::onPostItemClick)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_community_search)
 
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    finish()
+                    overridePendingTransition(0, 0)
+                }
+            }
+        )
+
         binding.apply {
             viewModel = communitySearchViewModel
             lifecycleOwner = this@CommunitySearchActivity
+            searchedPostRecyclerView.adapter = postListAdapter
+        }
+
+        binding.searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                communitySearchViewModel.searchPostAction()
+                return@setOnEditorActionListener true
+            }
+            false
         }
 
         communitySearchViewModel.error.observe(
@@ -35,13 +62,33 @@ class CommunitySearchActivity : BaseActivity() {
             this,
             EventObserver {
                 finish()
+                overridePendingTransition(0, 0)
+            }
+        )
+
+        communitySearchViewModel.searchedPostList.observe(this) { list ->
+            postListAdapter.submitList(list)
+        }
+
+        communitySearchViewModel.postItemClickEvent.observe(
+            this,
+            EventObserver { postId ->
+                activityLauncher.launch(PostDetailActivity.newIntent(this, postId))
+                setOpenActivityAnimation()
             }
         )
     }
 
-    override fun onPause() {
-        super.onPause()
-        overridePendingTransition(0, 0)
+    private val activityLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                communitySearchViewModel.searchPostAction()
+                return@registerForActivityResult
+            }
+        }
+
+    private fun onPostItemClick(item: CommunityPostModel) {
+        communitySearchViewModel.onPostItemClick(item)
     }
 
     companion object {
--- app/src/main/java/com/beside153/peopleinside/viewmodel/community/CommunitySearchViewModel.kt
+++ app/src/main/java/com/beside153/peopleinside/viewmodel/community/CommunitySearchViewModel.kt
@@ -3,20 +3,67 @@ package com.beside153.peopleinside.viewmodel.community
 import android.text.Editable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.beside153.peopleinside.base.BaseViewModel
+import com.beside153.peopleinside.model.community.post.CommunityPostModel
+import com.beside153.peopleinside.service.community.CommunityPostService
+import com.beside153.peopleinside.util.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CommunitySearchViewModel : BaseViewModel() {
+@HiltViewModel
+class CommunitySearchViewModel @Inject constructor(
+    private val communityPostService: CommunityPostService
+) : BaseViewModel() {
 
     private val _keyword = MutableLiveData("")
     val keyword: LiveData<String> get() = _keyword
 
+    private val _postItemClickEvent = MutableLiveData<Event<Long>>()
+    val postItemClickEvent: LiveData<Event<Long>> get() = _postItemClickEvent
+
+    private val _searchedPostList = MutableLiveData<List<CommunityPostModel>>(listOf())
+    val searchedPostList: LiveData<List<CommunityPostModel>> get() = _searchedPostList
+
+    private val _isSearched = MutableLiveData(false)
+    val isSearched: LiveData<Boolean> get() = _isSearched
+
+    private val _noResult = MutableLiveData(false)
+    val noResult: LiveData<Boolean> get() = _noResult
+
+    private var page = 1
+
+    fun searchPostAction() {
+        page = 1
+        _noResult.value = false
+        val word = _keyword.value ?: ""
+        if (word.isEmpty()) return
+
+        viewModelScope.launch(exceptionHandler) {
+            _searchedPostList.value = communityPostService.getCommunityPostList(page, word)
+            _isSearched.value = true
+
+            if (_searchedPostList.value.isNullOrEmpty()) {
+                _noResult.value = true
+            }
+        }
+    }
+
+    fun onPostItemClick(item: CommunityPostModel) {
+        _postItemClickEvent.value = Event(item.postId)
+    }
+
     fun afterKeywordTextChanged(editable: Editable?) {
         _keyword.value = editable.toString()
-//        loadSearchingTitle()
+        if (editable.isNullOrEmpty()) {
+            _isSearched.value = false
+            _noResult.value = false
+        }
     }
 
     fun onSearchCancelClick() {
         _keyword.value = ""
-//        initSearchScreen()
+        _isSearched.value = false
     }
 }
