d13df0b5e7b4d42472242bb109ee49ca2a3b2f52
hong <sdhong0609@gmail.com>
2023-08-18 23:56:53 +0900

    refactor: version code 업데이트

--- buildSrc/src/main/java/com/beside153/peopleinside/Configuration.kt
+++ buildSrc/src/main/java/com/beside153/peopleinside/Configuration.kt
@@ -8,5 +8,5 @@ object Configuration {
     const val minorVersion = 1
     const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 17
+    const val versionCode = 18
 }
